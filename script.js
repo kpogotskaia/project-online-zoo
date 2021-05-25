a1e6d2c0d88842cad4d50dbc4bd556c6c155336e
Krystina Pogotskaia <wal95kristina@mail.ru>
2021-05-25 08:40:05 -0700

    add new background for first screeen; edit background button; add new section - four

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 import * as mdb from 'mdb-ui-kit'; 
+
+// Получить элемент контейнера
+var btnContainer = document.getElementsByClassName("header__menu-list");
+
+// Сделать все кнопки с class="btn" внутри контейнера
+var btns = btnContainer.getElementsByClassName("header__link");
+
+// Выполните цикл по кнопкам и добавьте активный класс к текущей/нажатой кнопке
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
