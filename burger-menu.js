53653dad0f00b47f46e6f3ee4a3438bec705bdbf
Krystina Pogotskaia <wal95kristina@mail.ru>
2021-08-11 16:27:15 -0700

    feat: section-one and two for a tablet

--- burger-menu.js
+++ burger-menu.js
@@ -1,1 +1,7 @@
 
+$(document).ready(function() {
+	$('.header__burger').click(function() {
+		$('.header__burger').toggleClass('open-menu');
+		$('.header__menu').toggleClass('open-menu');
+	});
+  });
