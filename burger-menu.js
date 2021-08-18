c8ad1f79172bccf99f080da5c21a263e93969719
Krystina Pogotskaia <wal95kristina@mail.ru>
2021-08-18 11:32:40 -0700

    feat: burger-menu

--- burger-menu.js
+++ burger-menu.js
@@ -1,7 +1,17 @@
+var menuBtn = document.getElementById('menuBtn');
+var menuContainer = document.getElementById('menuContainer');
 
-$(document).ready(function() {
-	$('.header__burger').click(function() {
-		$('.header__burger').toggleClass('open-menu');
-		$('.header__menu').toggleClass('open-menu');
-	});
-  });
+var menuIconClosed = "header__menu-icon closed"; //class name for closed button
+var menuIconOpened = "header__menu-icon opened"; //class name for opened button
+var menuContClosed = "header__menu-container closed"; //class name for closed menu
+var menuContOpened = "header__menu-container opened"; //class name for opened menu
+
+menuBtn.onclick = function() {
+  if (menuBtn.className == menuIconClosed) {
+    menuBtn.className = menuIconOpened;
+    menuContainer.className = menuContOpened;
+  } else if (menuBtn.className == menuIconOpened) {
+    menuBtn.className = menuIconClosed;
+    menuContainer.className = menuContClosed;
+  }
+}
