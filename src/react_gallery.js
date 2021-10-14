7d8f156312c028572f2d2b0b0d011cbdf18e5441
Krystina Pogotskaia <wal95kristina@mail.ru>
2021-10-14 12:44:53 -0700

    feat: react

---
+++ src/react_gallery.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class reactGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      <button onClick={() => this.setState({ liked: true }) }>
+        Like
+      </button>
+    );
+  }
+}
+
+let domContainer = document.querySelector('#react_gallery');
+ReactDOM.render(<reactGallery />, domContainer);
