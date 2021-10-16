dbeb58069807f915414e5a13b6d1f1bbe0d30114
Krystina Pogotskaia <wal95kristina@mail.ru>
2021-10-15 18:24:18 -0700

    feat: react - fix section one

--- src/section-one/SectionOne.js
+++ src/section-one/SectionOne.js
@@ -16,73 +16,73 @@ import VKIcon from '../assets/sociales/vk.png';
 export const SectionOne = () => {
   return (
     <section className="section-one">
-    <div className="container">
-        <h1 className="section-one__heading">Watch your favorite animal online</h1>
+      <div className="container">
+          <h1 className="section-one__heading">Watch your favorite animal online</h1>
 
-        <Button />
+          <Button />
 
-        <div className="section-one__sociales">
-            <a href="" className="instagram">
-                <img src={InstagramIcon} alt="instagram" />
-            </a>
-            <a href="" className="twitter">
-                <img src={TwitterIcon} alt="twitter" />
-            </a>
-            <a href="" className="youtube">
-                <img src={YoutubeIcon} alt="youtube" />
-            </a>
-            <a href="" className="vk">
-                <img src={VKIcon} alt="vk" />
-            </a>
-        </div>
-    </div>
+          <div className="section-one__sociales">
+              <a href="" className="instagram">
+                  <img src={InstagramIcon} alt="instagram" />
+              </a>
+              <a href="" className="twitter">
+                  <img src={TwitterIcon} alt="twitter" />
+              </a>
+              <a href="" className="youtube">
+                  <img src={YoutubeIcon} alt="youtube" />
+              </a>
+              <a href="" className="vk">
+                  <img src={VKIcon} alt="vk" />
+              </a>
+          </div>
 
-    <div className="section-one__gallery">
-        <div className="section-one__gallery-imgs">
-            <div>
-                <img className="img" src={EagleImg} alt="eagle" />
-            </div>
-            <div className="img-active">
-                <img className="img-active__img" src={PandaImg} alt="" />
-                <p className="img-active__pharagraph"> Panda’s name is Bei Bei. He
-                    is 2 years old. Bei Bei is
-                    from China. He loves bamboos.
-                </p>
-                <div className="img-active__btn">
-                    <button className="button">watch online
-                        <div className="button__play-icon">
-                            <span></span>
-                        </div>
-                    </button>
-                    <button className="button button-donate">donate
-                        <div className="button__play-icon">
-                            $
-                        </div>
-                    </button>
-                </div>
-            </div>
-            <div>
-                <img className="img" src={MonkeyImg} alt="monkey" />
-            </div>
-            <div>
-                <img className="img" src={CrocImg} alt="croc" />
-            </div>
-            <div>
-                <img className="img" src={FoxImg} alt="fox" />
-            </div>
-        </div>
-        <div className="section-one__gallery-line">
-            <div className="gallery__line-ellipse">
-                <div></div>
-            </div>
-        </div>
-        <div className="section-one__gallery-scroll">
-            <span>02 /</span>
-            <span>08</span>
-            <div className="section-one__gallery-scroll-line">
-                <div className="gallery__scroll-button"></div>
-            </div>
-        </div>
+      <div className="section-one__gallery">
+          <div className="section-one__gallery-imgs">
+              <div>
+                  <img className="img" src={EagleImg} alt="eagle" />
+              </div>
+              <div className="img-active">
+                  <img className="img-active__img" src={PandaImg} alt="panda" />
+                  <p className="img-active__pharagraph"> Panda’s name is Bei Bei. He
+                      is 2 years old. Bei Bei is
+                      from China. He loves bamboos.
+                  </p>
+                  <div className="img-active__btn">
+                      <button className="button-active">watch online
+                          <div className="button__play-icon">
+                              <span></span>
+                          </div>
+                      </button>
+                      <button className="button-active button-donate">donate
+                          <div className="button__play-icon">
+                              $
+                          </div>
+                      </button>
+                  </div>
+              </div>
+              <div>
+                  <img className="img" src={MonkeyImg} alt="monkey" />
+              </div>
+              <div>
+                  <img className="img" src={CrocImg} alt="croc" />
+              </div>
+              <div>
+                  <img className="img" src={FoxImg} alt="fox" />
+              </div>
+          </div>
+          <div className="section-one__gallery-line">
+              <div className="gallery__line-ellipse">
+                  <div></div>
+              </div>
+          </div>
+          <div className="section-one__gallery-scroll">
+              <span>02 /</span>
+              <span>08</span>
+              <div className="section-one__gallery-scroll-line">
+                  <div className="gallery__scroll-button"></div>
+              </div>
+          </div>
+      </div>
     </div>
 </section>
   );
