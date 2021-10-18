687c620d1922540150914392c2534665cbe8d875
Krystina Pogotskaia <wal95kristina@mail.ru>
2021-10-17 17:51:10 -0700

    feat: fix code section Pets in Zoo

--- src/section-three/PetsInZoo.js
+++ src/section-three/PetsInZoo.js
@@ -6,8 +6,8 @@ import ThreeSliderPandaImg from '../assets/treeScreen/panda.png';
 import ThreeSliderEagleImg from '../assets/treeScreen/eagle.png';
 import ThreeSliderMonkeyImg from '../assets/treeScreen/monkey.png';
 import ThreeSliderCrocImg from '../assets/treeScreen/kro.png';
-import ArrowRightImg from'../assets/rightArr.png';
-import ArrowLeftImg from '../assets/leftArr.png';
+//import ArrowRightImg from'../assets/arrRight.png';
+//import ArrowLeftImg from '../assets/arrLeft.png';
 
 export const PetsInZoo = () => {
   return (
@@ -15,9 +15,6 @@ export const PetsInZoo = () => {
       <div className="container">
         <h3 className="section-three__heading">Pets in Zoo</h3>
         <div className="section-three__slider">
-          <span className="section-three__left-button">
-              <img src={ArrowRightImg} alt="left-arrow" />
-          </span>
           <div className="section-three__slider-active img">
             <img src={ThreeSliderPandaImg} alt="panda" />
             <p>Panda’s name is Bei Bei. He
@@ -25,12 +22,12 @@ export const PetsInZoo = () => {
                 He loves bamboos.
             </p>
             <div className="section-three__slider-active__btn">
-                <button className="button">watch online
+                <button className="button-active">watch online
                     <div className="button__play-icon">
                         <span></span>
                     </div>
                 </button>
-                <button className="button button-donate">donate
+                <button className="button-active button-donate">donate
                     <div className="button__play-icon">
                         $
                     </div>
@@ -46,9 +43,6 @@ export const PetsInZoo = () => {
           <div className="section-three__slider-crocodille img">
               <img src={ThreeSliderCrocImg} alt="crocodille" />
           </div>
-          <span className="section-three__right-button">
-              <img src={ArrowLeftImg} alt="right-arrow" />
-            </span>
         </div>
         <div className="gallery-scroll">
             <span>01/</span>
