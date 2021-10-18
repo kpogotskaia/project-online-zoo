85edf4de0f777057ee7aadbb410da66717490fa2
Krystina Pogotskaia <wal95kristina@mail.ru>
2021-10-18 10:26:49 -0700

    delit folder my-app

--- my-app/src/App.js
+++
@@ -1,7 +0,0 @@
-import { HomePage } from './HomePage';
-import { CountryZoo } from './CountryZoo';
-import { ChooseFavorite } from './ChooseFavorite';
-
-import './App.scss';
-
-export const App = () => <HomePage />;
