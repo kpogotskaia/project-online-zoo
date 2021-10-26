75dfc40c328b31ea5c0e2a63796abd961d9f9704
ARTSIOM KUKHAREV <xeontem@gmail.com>
2021-10-25 18:37:14 -0700

    feat: build

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { HomePage } from './home-page/HomePage';
-import { CountryZoo } from './country-zoo/CountryZoo';
-import { ChooseFavorite } from './choose-favorite/ChooseFavorite';
-
-import './App.scss';
-
-export const App = () => <HomePage />;
