06e67f27b5c0f77601cd2910c19b291368ff9d83
ARTSIOM KUKHAREV <xeontem@gmail.com>
2022-04-28 20:16:37 -0700

    refactor: aside slider

--- src/components/sliders/countryZooAside/CountryZooLeftAside.js
+++ src/components/sliders/countryZooAside/CountryZooLeftAside.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { LEFT_MENU_ARROW, ANIMALS} from '../../../constants';
+import { ANIMALS} from '../../../constants';
 import { clamp } from '../../../utils';
 import classes from './CountryZooLeftAside.module.scss';
 
@@ -10,79 +10,46 @@ const reArrangeItems = (arr, shift) =>
   }, []);
 
 export const CountryZooLeftAside = () => {
-
-  const [selectedSlideIndex, setSelectedSlideIndex] = useState(1);
   const [slides, setSlides] = useState(ANIMALS);
-
-  const [offset, setOffset] = useState(0);
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const changeSlideIndex = (shift) => {
-    setActiveIndex(clamp(activeIndex, shift, 0, ANIMALS.length));
-  };
-
-  let curOffset = offset;
 
   return (
     <aside className={classes['left-menu']}>
-      <div className={`
-        ${classes['left-menu__imgs']}
-        ${classes['slidesMove']}
-        `}
-        style={{bottom: `-${offset * activeIndex}px`}}
-        >
-
-        {slides.map((animals, i) => (
-          <div
-            key={animals.alt}
-            className={`
-            ${classes['left-menu__animal']}
-            ${i === 0 ? classes['left-menu--active'] : ''}
-            `}
-            style={{backgroundImage: `url('${animals.imgUrl}'`}}
-            onClick={() => {
-              if (0 !== i) {
-                const shift = i === 0 ? 1 : -i;
-                const reArranged = reArrangeItems(slides, shift);
-                const selectedInd = ANIMALS.findIndex(a => a.alt === reArranged[0].alt);
-                setSlides(reArranged);
-                setSelectedSlideIndex(selectedInd);
-              }
-            }}
-
-            ref={e => {
-              if (e) {
-                // 33 - vertical margin
-                const nextOffset = e.offsetWidth + 33;
-
-                if (curOffset !== nextOffset) {
-                  setOffset(nextOffset);
-                  curOffset = nextOffset;
+      <div className={classes['left-menu__imgs']}>
+        <div>
+          {slides.map((animals, i) => (
+            <div
+              key={animals.alt}
+              className={`
+                ${classes['slide']}
+                ${i === 0 ? classes['slide--active'] : ''}
+              `}
+              onClick={() => {
+                if (i !== 0) {
+                  setSlides(reArrangeItems(slides, -i));
                 }
-              }
-            }}
-          >
-          </div>
-        ))}
+              }}
+            >
+              <div style={{backgroundImage: `url('${animals.imgUrl}'`}}></div>
+            </div>
+          ))}
+
+          <div
+            className={`
+              ${classes['arrow']}
+              ${classes['arrow-top']}
+            `}
+            onClick={e => setSlides(reArrangeItems(slides, 1))}
+          ></div>
+
+          <div
+            className={`
+              ${classes['arrow']}
+              ${classes['arrow-bottom']}
+            `}
+            onClick={e => setSlides(reArrangeItems(slides, -1))}
+          ></div>
+        </div>
       </div>
-
-      <div
-        className={`
-        ${classes['arrow']}
-        ${classes['arrow-top']}
-        `}
-        onClick={e => changeSlideIndex(-1)}
-      ></div>
-
-        <div
-          className={`
-          ${classes['arrow']}
-          ${classes['arrow-bottom']}
-          `}
-          onClick={e => changeSlideIndex(1)}
-        ></div>
-  </aside>
+    </aside>
   );
 };
-
-
